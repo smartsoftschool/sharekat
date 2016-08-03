31cab66738dcf8c2700a2f83a23d2bdb74fc6ece
Mohamed Lotfy <Mohamed Lotfy>
2016-08-04 00:26:05 +0200

    04--8-16-1

--- routes/signup.js
+++ routes/signup.js
@@ -1,5 +1,5 @@
 module.exports = function (app) {
     app.get('/signup', function (req, res) {
-        res.render('users/signup', { title: 'login' });
+        res.render('users/signup', { title: 'Login' });
     });
 }
