cb8100e22b95a055c95a87c8a1d750d03c7c9fb3
ma7amad anwar <ma7amad anwar>
2016-08-07 12:19:20 +0200

    7/8/16

--- routes/signup.js
+++ routes/signup.js
@@ -2,4 +2,7 @@ module.exports = function (app) {
     app.get('/signup', function (req, res) {
         res.render('users/signup', { title: 'login' });
     });
+    app.get('/login', function (req, res) {
+        res.render('users/login', { title: 'login' });
+    });
 }
